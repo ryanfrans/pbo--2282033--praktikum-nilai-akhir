7b97c3050dfe4bbc9b3ab4d8a0c4e31b6e98d930
ryanfrans <2282033@unai.edu>
2026-09-21 09:42:22 +0700

    menambahkan variabel perintah

--- src/Nilaiakhir.java
+++ src/Nilaiakhir.java
@@ -1,5 +1,20 @@
-public class Main {
+import java.util.Scanner;
+
+public class Nilaiakhir {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scanner - new Scanner(System.int);
+
+        System.out.print("Nilai praktikum" );
+        double nilaiPraktikum = scanner.nextDouble();
+
+        System.out.print("Nilai tugas" );
+        double nilaiTugas = scanner.nextDouble();
+
+        System.out.print("Nilai MID" );
+        double nilaiMID = scanner.nextDouble();
+
+        System.out.print("Nilai final" );
+        double nilaiFinal = scanner.nextDouble();
     }
 }
